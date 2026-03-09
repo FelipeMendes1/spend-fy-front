44289144e202a852a90b27bacad7ce59c4d04eb2
Lucas Tchaikovsky dos Santos Rodrigues <lucastchaikovsky@hotmail.com>
2026-03-09 19:03:13 -0300

    Add new routes for Transacoes and Categorias

--- spendfy-front/src/routes/index.jsx
+++ spendfy-front/src/routes/index.jsx
@@ -3,7 +3,9 @@ import { Routes, Route, Navigate } from 'react-router-dom';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 import Dashboard from '../pages/Dashboard';
+import Transacoes from '../pages/Transacoes';
 import TransactionForm from '../pages/TransactionForm';
+import Categorias from '../pages/Categorias';
 import PrivateRoute from './PrivateRoute';
 import Layout from '../components/Layout';
 
@@ -17,16 +19,17 @@ export default function AppRoutes() {
       {/* Grupo de Rotas Privadas */}
       <Route element={<PrivateRoute />}>
         <Route element={<Layout />}>
-            <Route path="/dashboard" element={<Dashboard />} />
-            <Route path="/transacoes/nova" element={<TransactionForm />} />
-            <Route path="/categorias" element={<div>Página de Categorias</div>} />
-            {/* rotas privadas */}
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/transacoes" element={<Transacoes />} />
+          <Route path="/transacoes/nova" element={<TransactionForm />} />
+          <Route path="/transacoes/editar/:id" element={<TransactionForm />} />
+          <Route path="/categorias" element={<Categorias />} />
+          {/* Adicione novas rotas privadas aqui */}
         </Route>
       </Route>
 
       {/* Redirecionamento padrão */}
       <Route path="*" element={<Navigate to="/dashboard" />} />
-
     </Routes>
   );
 }
